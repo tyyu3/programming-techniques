c46566eeb35012e5d64eec5dcbe5aae69a05b8c4
Igor Shcherbakov <ischerbakov@ptsecurity.com>
2020-01-30 23:06:11 +0300

    Added SQliteCpp library

--- Sorting Technique/Sorting Technique/Sorting Technique.cpp
+++ Sorting Technique/Sorting Technique/Sorting Technique.cpp
@@ -2,13 +2,21 @@
 //
 
 #include "Sorting Technique.h"
+#include "./SQLiteCpp/SQLiteCpp.h"
 #include "CampEntry.cpp"
 using namespace std;
 
 int main()
 {
-    CampEntry cr(2000, 1, 1, "Igor", {1,2,1990});
-    cout << cr << '\n';
-	cout << "Hello CMake." << endl;
-	return 0;
+    CampEntry cr(2000, 1, 1, "Igor", {1, 2, 1990});
+    try
+    {
+        SQLite::Database db("example.db3");
+    }
+    catch (std::exception& e)
+    {
+        std::cout << "exception: " << e.what() << std::endl;
+    }
+    cout << "Hello CMake." << endl;
+    return 0;
 }
