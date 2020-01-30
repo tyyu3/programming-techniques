d651ddf13aca16f14d253c415799005e7024d4d4
Igor Shcherbakov <ischerbakov@ptsecurity.com>
2020-01-30 20:30:53 +0300

    Moved project to CMake

---
+++ Sorting Technique/Sorting Technique/Sorting Technique.cpp
@@ -0,0 +1,14 @@
+﻿// Sorting Technique.cpp : Defines the entry point for the application.
+//
+
+#include "Sorting Technique.h"
+#include "CampEntry.cpp"
+using namespace std;
+
+int main()
+{
+    CampEntry cr(2000, 1, 1, "Igor", {1,2,1990});
+    cout << cr << '\n';
+	cout << "Hello CMake." << endl;
+	return 0;
+}
