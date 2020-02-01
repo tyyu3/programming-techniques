dd85ec209bb570731a89f3be989cffa37e401970
Igor Shcherbakov <ischerbakov@ptsecurity.com>
2020-02-01 14:54:20 +0300

    Move thirdparty/ up the repo structure

---
+++ Search Technique/Search Technique/Search Technique.cpp
@@ -0,0 +1,12 @@
+﻿// Search Technique.cpp : Defines the entry point for the application.
+//
+
+#include "Search Technique.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
